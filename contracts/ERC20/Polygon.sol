269cf1c1423ddfef6bd9184decf1e334503e097c
Mona <tigerzmona@yahoo.com>
2022-04-05 21:59:03 -0400

    notes on contract

--- contracts/ERC20/Polygon.sol
+++ contracts/ERC20/Polygon.sol
@@ -8,4 +8,10 @@ contract FireToken is ERC20 {
     constructor() ERC20("FireToken", "LIT") {
         _mint(msg.sender, 1000 * 10 ** decimals());
     }
+
+    //NOTE:
+    //All operations in the smart contract use the token base units, 
+    //so to transfer 100 tokens you transfer 100 * 10 ** 18 token base units
+
+    //decimals() --> returns uint8 18 as value
 }
