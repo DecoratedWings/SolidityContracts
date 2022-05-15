df29f7479b1361805251811cbc4fe001eb40d704
Mona <tigerzmona@yahoo.com>
2022-05-15 18:04:01 -0400

    basic CRUD practice

---
+++ contracts/CRUD.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.8.7;
+
+contract CRUD {
+    uint[] public arr;
+    mapping(uint=>string) map;
+
+    //CRUD ARRAY:
+    function createEntryForArray(uint entry) external {
+        arr.push(entry);
+    }
+    function readFromArray(uint index) external view returns(uint){
+        return arr[index];
+    }
+    function updateArray(uint index, uint value) external {
+        arr[index] = value;
+    }
+    function deleteFromArray(uint index) external{
+        delete arr[index];
+    }
+
+    //CRUD MAPPING : 
+
+    function createOrUpdateMap(uint key, string memory val) external {
+        map[key] = val;
+    }
+
+    function readMap(uint key) external view returns(string memory){
+        return map[key];
+    }
+
+    function deleteFromMap(uint key) external {
+        delete map[key];
+    }
+
+
+}
